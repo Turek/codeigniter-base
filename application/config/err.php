19bf11cc19306a68c0df3f5d53dc48e2523d9e1b
Tomasz Turczynski <tomasz@turczynski.com>
2014-01-28 21:46:25 +0000

    Adding somre comments to config file.

--- application/config/err.php
+++ application/config/err.php
@@ -1,26 +1,29 @@
 <?php
 
-// Output errors to browser - TRUE = silent mode, only e-mails will be sent
+// Should errors not be displayed for the user.
 define('ERR_SILENT', FALSE);
 
-// Where to send the error report. Leave blank for nowhere
+// E-mail recipient.
 define('ERR_MAIL_TO', 'webmaster@domain.com');
 define('ERR_MAIL_TO_NAME', 'Webmaster');
 
+// E-mail from.
 define('ERR_MAIL_FROM', 'webmaster@domain.com');
 define('ERR_MAIL_FROM_NAME', strtoupper(ENVIRONMENT) . ' Error Daemon');
+
+// E-mail subject
 define('ERR_MAIL_SUBJECT', SITE_NAME . ': [TYPE] @ [BASENAME]:[LINE]');
 
 // SMTP, or MAIL otherwise
 define('ERR_MAIL_METHOD', 'MAIL');
 
+// SMTP details.
 define('SMTP_HOST', 'localhost');
 define('SMTP_USER', 'g4send');
 define('SMTP_PASS', 's3ndm41l');
 define('SMTP_PORT', 25);
 
-// The subject of the mail. Supported tags: [TYPE], [FILE], [LINE], [STRING], [BASENAME]
-// Other things of interest to include in the email. CSV of supported values: POST, GET, SERVER, GLOBALS, SESSION
+// What information should be also attached to error e-mails.
 define('ERR_MAIL_FOOTERS', 'POST,GET,SESSION');
 
 $config['err_silent'] = FALSE;
